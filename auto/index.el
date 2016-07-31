b1fb9bf613f048e276b54cc2a9be4546e28990a8
templeofthedog <ccarley@gmail.com>
2016-07-31 15:58:19 -0400

    Add files via upload

---
+++ auto/index.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "index"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "latin1") ("fontenc" "T1") ("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-2"
+    "sec-3"
+    "sec-4"
+    "sec-5"))
+ :latex)
+
